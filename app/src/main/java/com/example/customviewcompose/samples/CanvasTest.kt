28752a808ee6c3916c1dbdce370e398695b811c6
sumin93 <suminfx@mail.ru>
2023-09-01 01:02:24 +0300

    Draw path

--- app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
@@ -6,33 +6,57 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun CanvasTest() {
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color.Cyan, Color.Magenta),
+                    start = Offset(0f, 100.dp.toPx()),
+                    end = Offset(100.dp.toPx(), 100.dp.toPx()),
+                    tileMode = TileMode.Mirror
+                )
+            )
     ) {
-        drawLine(
+        drawPath(
+            path = Path().apply {
+                moveTo(center.x, 100.dp.toPx())
+                lineTo(center.x + 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x + 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x + 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x, 220.dp.toPx())
+                lineTo(center.x - 60.dp.toPx(), 250.dp.toPx())
+                lineTo(center.x - 45.dp.toPx(), 195.dp.toPx())
+                lineTo(center.x - 75.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x - 25.dp.toPx(), 150.dp.toPx())
+                lineTo(center.x, 100.dp.toPx())
+            },
             color = Color.White,
-            start = Offset(0f, 0f),
-            end = Offset(size.width, size.height),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawLine(
-            color = Color.White,
-            start = Offset(size.width, 0f),
-            end = Offset(0f, size.height),
-            strokeWidth = 1.dp.toPx()
-        )
-        drawCircle(
-            color = Color.White,
-            radius = 100.dp.toPx(),
-            style = Stroke(width = 1.dp.toPx())
+            style = Fill
         )
     }
+}
+
+@Composable
+fun Dp.toPx() = with(LocalDensity.current) {
+    this@toPx.toPx()
+}
+
+@Composable
+fun convertDpToPx(dp: Dp): Float {
+    return LocalDensity.current.density * dp.value
 }
--- app/src/main/java/com/example/customviewcompose/samples/HomeWork.kt
+++ app/src/main/java/com/example/customviewcompose/samples/HomeWork.kt
@@ -1,7 +1,14 @@
 package com.example.customviewcompose.samples
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
@@ -67,4 +74,36 @@ fun DrawScope.oleg() {
         start = Offset(190.dp.toPx(), 25.dp.toPx()),
         end = Offset(190.dp.toPx(), 75.dp.toPx())
     )
+}
+
+@Composable
+fun House() {
+    Canvas(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.linearGradient(colors = listOf(Color.Magenta, Color.Blue)))
+    ) {
+        drawPath(
+            path = Path().apply {
+                moveTo(100.dp.toPx(), 300.dp.toPx())
+                lineTo(100.dp.toPx(), 500.dp.toPx())
+                lineTo(300.dp.toPx(), 500.dp.toPx())
+                lineTo(300.dp.toPx(), 300.dp.toPx())
+                lineTo(200.dp.toPx(), 200.dp.toPx())
+                lineTo(100.dp.toPx(), 300.dp.toPx())
+                lineTo(300.dp.toPx(), 300.dp.toPx())
+                moveTo(150.dp.toPx(), 350.dp.toPx())
+                lineTo(250.dp.toPx(), 350.dp.toPx())
+                lineTo(250.dp.toPx(), 450.dp.toPx())
+                lineTo(150.dp.toPx(), 450.dp.toPx())
+                lineTo(150.dp.toPx(), 350.dp.toPx())
+                moveTo(200.dp.toPx(), 350.dp.toPx())
+                lineTo(200.dp.toPx(), 450.dp.toPx())
+                moveTo(150.dp.toPx(), 400.dp.toPx())
+                lineTo(250.dp.toPx(), 400.dp.toPx())
+            },
+            color = Color.White,
+            style = Stroke(width = 4.dp.toPx())
+        )
+    }
 }
