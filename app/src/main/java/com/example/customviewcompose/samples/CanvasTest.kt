25360d7de52a13d81d064e7ed4838d7219540137
sumin93 <suminfx@mail.ru>
2023-09-01 19:26:44 +0300

    Drag gestures

--- app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
+++ app/src/main/java/com/example/customviewcompose/samples/CanvasTest.kt
@@ -2,54 +2,71 @@ package com.example.customviewcompose.samples
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun CanvasTest() {
+    var points by rememberSaveable {
+        mutableStateOf<List<Point>>(listOf())
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(Color.Cyan, Color.Magenta),
-                    start = Offset(0f, 100.dp.toPx()),
-                    end = Offset(100.dp.toPx(), 100.dp.toPx()),
-                    tileMode = TileMode.Mirror
+            .background(Color.White)
+            .pointerInput(key1 = Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                        points = points + Point(offset = it, isStartedPosition = true)
+                    },
+                    onDrag = { change, _ ->
+                        points = points + change.historical.map {
+                            Point(offset = it.position, isStartedPosition = false)
+                        }
+                    }
                 )
-            )
+            }
     ) {
+        val path = Path()
+        points.forEach {
+            if (it.isStartedPosition) {
+                path.moveTo(it.offset.x, it.offset.y)
+            } else {
+                path.lineTo(it.offset.x, it.offset.y)
+            }
+        }
         drawPath(
-            path = Path().apply {
-                moveTo(center.x, 100.dp.toPx())
-                lineTo(center.x + 25.dp.toPx(), 150.dp.toPx())
-                lineTo(center.x + 75.dp.toPx(), 150.dp.toPx())
-                lineTo(center.x + 45.dp.toPx(), 195.dp.toPx())
-                lineTo(center.x + 60.dp.toPx(), 250.dp.toPx())
-                lineTo(center.x, 220.dp.toPx())
-                lineTo(center.x - 60.dp.toPx(), 250.dp.toPx())
-                lineTo(center.x - 45.dp.toPx(), 195.dp.toPx())
-                lineTo(center.x - 75.dp.toPx(), 150.dp.toPx())
-                lineTo(center.x - 25.dp.toPx(), 150.dp.toPx())
-                lineTo(center.x, 100.dp.toPx())
-            },
-            color = Color.White,
-            style = Fill
+            path = path,
+            brush = Brush.linearGradient(listOf(Color.Cyan, Color.Magenta, Color.Red)),
+            style = Stroke(width = 10.dp.toPx())
         )
     }
 }
+
+data class Point(
+    val offset: Offset,
+    val isStartedPosition: Boolean
+)
 
 @Composable
 fun Dp.toPx() = with(LocalDensity.current) {
